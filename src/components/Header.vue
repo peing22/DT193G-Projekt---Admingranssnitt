c771a6d6fd872814a9f637ab9abaec452cd0e046
Petra Ingemarsson <petra.ingemarsson@gmail.com>
2023-10-12 11:33:02 +0200

    Bakgrundsbild och ikonkit samt CSS för header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,18 @@
 <template>
     <header>
-        <p class="text-green-700">{{ message }}</p>
-        <button @click="logoutUser()">Logga ut</button>
-        <nav>
-            <ul>
-                <li>
-                    <RouterLink to="/produkter">Produkter</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/administrera">Administrera</RouterLink>
-                </li>
-            </ul>
+        <div class="flex items-center justify-end p-2 text-xs md:text-base">
+            <p class="mx-2">{{ message }}</p>
+            <button @click="logoutUser()"
+                class="bg-white/20 transition duration-300 ease-in-out hover:bg-white/40 py-1 px-2 md:px-4 uppercase border-solid border-y border-white/20 font-medium tracking-widest rounded-full">Logga ut<i class="fa-solid fa-right-from-bracket ps-1"></i></button>
+        </div>
+        <nav
+            class="bg-white/20 flex items-center justify-center py-3 md:py-7 mx-auto uppercase tracking-widest text-xs md:text-xl">
+            <RouterLink to="/produkter"
+                class="border-solid border-y border-white/20 transition duration-300 ease-in-out hover:border-white/60 p-2 md:p-4 mx-3 md:mx-10"
+                :class="{ active: currentRoute === '/produkter' }">Produkter</RouterLink>
+            <RouterLink to="/administrera"
+                class="border-solid border-y border-white/10 transition duration-300 ease-in-out hover:border-white/60 p-2 md:p-4 mx-3 md:mx-10"
+                :class="{ active: currentRoute === '/administrera' }">Administrera</RouterLink>
         </nav>
     </header>
 </template>
@@ -23,7 +25,8 @@ import { useAuthStore } from '../stores/auth'
 export default {
     data() {
         return {
-            message: "Inloggad som " + useAuthStore().$state.name
+            message: "Inloggad som " + useAuthStore().$state.name,
+            currentRoute: ""
         }
     },
     components: {
@@ -31,6 +34,18 @@ export default {
     },
     computed: {
         ...mapWritableState(useAuthStore, ['logoutUser'])
+    },
+    watch: {
+        $route(to) {
+            this.currentRoute = to.path
+        }
     }
 }
 </script>
+
+<style scoped>
+.active {
+    border-top: solid 1px rgba(255, 255, 255, 0.6);
+    border-bottom: solid 1px rgba(255, 255, 255, 0.6);
+}
+</style>
